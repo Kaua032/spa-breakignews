78d78016b0afec8f83195d73789c410d37357611
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-20 18:30:09 -0300

    feat: Deleted the README.md file.

---
+++ spa-breakignews/src/Datas.js
@@ -0,0 +1,42 @@
+export const news = [
+    {
+        title: "Notícia 1",
+        text: "Lorem Ipsumasdlçfjaslçdfjaslkçdfjsaldf",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        banner: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 2",
+        text: "Lorem Ipsumasdlçfjaslçdfjaslkçdfjsaldf",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        banner: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 3",
+        text: "Lorem Ipsumasdlçfjaslçdfjaslkçdfjsaldf",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        banner: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 4",
+        text: "Lorem Ipsumasdlçfjaslçdfjaslkçdfjsaldf",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        banner: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        likes: 2,
+        comments: 4,
+    },
+    {
+        title: "Notícia 5",
+        text: "Lorem Ipsumasdlçfjaslçdfjaslkçdfjsaldf",
+        image: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        banner: "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fwp-content%2Fuploads%2F2022%2F06%2Fimage-search-1600-x-840-px-62c6dc4ff1eee-sej.png&tbnid=Pso-9ayGpy0KlM&vet=12ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.searchenginejournal.com%2Fbest-image-search-engines%2F299963%2F&docid=faQEvyb6ULMOUM&w=1600&h=840&itg=1&q=image&ved=2ahUKEwiIxuywkLqBAxWQB7kGHfunDG4QMygBegQIARBP",
+        likes: 2,
+        comments: 4,
+    },
+]
