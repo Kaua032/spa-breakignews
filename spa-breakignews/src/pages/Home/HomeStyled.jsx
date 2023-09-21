29b8def69c59eb0438c012ce89e560f2186018b3
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-21 20:54:03 -0300

    feat: Update news data and add styling to the cards

---
+++ spa-breakignews/src/pages/Home/HomeStyled.jsx
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const HomeBody = styled.section`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-gap: 1rem;
+  margin: 0 auto;
+  width: 80%;
+`;
