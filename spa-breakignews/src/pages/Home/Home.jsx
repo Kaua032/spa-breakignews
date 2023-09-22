95272585b7a8767823c12f85f55e9ad95b715146
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-21 21:32:40 -0300

    feat: Update dependencies, add axios, follow redirects, form-data, proxy-from-env, and mime-db.

--- spa-breakignews/src/pages/Home/Home.jsx
+++ spa-breakignews/src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
 import { Card } from "../../components/Cards/Card";
 import { Navbar } from "../../components/Navbar/Navbar";
 import { news } from "../../Datas.js";
+import { getAllNews } from "../../services/newsServices";
 import { HomeBody } from "./HomeStyled";
 
 export default function Home() {
+
+  async function findAllNews() {
+    const response = await getAllNews();
+
+    console.log(response);
+  } 
+
+  findAllNews();
+
   return (
     <>
       <Navbar />
---
+++ spa-breakignews/src/services/newsServices.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:3500';
+
+export function getAllNews() {
+    const response = axios.get(`${baseURL}/news/all?`);
+    return response;
+};
