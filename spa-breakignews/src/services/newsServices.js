8cf4131333edaf54a06a4b192bc9aa8e8ccf6f2d
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-23 13:35:58 -0300

    feat: The commit changes the URL in the getAllNews function to add query parameters for pagination, limiting the number of results to 10 and starting at offset 0.

--- spa-breakignews/src/services/newsServices.js
+++ spa-breakignews/src/services/newsServices.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 const baseURL = 'http://localhost:3500';
 
 export function getAllNews() {
-    const response = axios.get(`${baseURL}/news/all`);
+    const response = axios.get(`${baseURL}/news/all?limit=10&offset=0`);
     return response;
 };
