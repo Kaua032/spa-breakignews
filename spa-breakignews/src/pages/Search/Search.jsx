7b002ce2b956df1ff74ab2a3560cd23d121df6ee
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-27 23:24:45 -0300

    feat: Added search functionality to the news app.

--- spa-breakignews/src/pages/Search/Search.jsx
+++ spa-breakignews/src/pages/Search/Search.jsx
@@ -1,7 +1,52 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { searchNews } from "../../services/newsServices";
+import { ContainerResults, SearchNews, TextResults } from "./SearchStyled";
+import { Card } from "../../components/Cards/Card";
 
 export default function Search() {
   const { title } = useParams();
+  const [news, setNews] = useState([]);
 
-  return <h1>{title}</h1>;
+  async function search() {
+    try {
+      const newsApi = await searchNews(title);
+      setNews(newsApi.data.foundNews);
+    } catch (error) {
+      console.log(error);
+      setNews([]);
+    }
+  }
+
+  useEffect(() => {
+    search();
+  }, [title]);
+
+  return (
+    <ContainerResults>
+      <TextResults>
+        <span>
+          {news.length !== 0
+            ? `Encontramos ${news.length} ${
+                news.length > 1 ? "resultados" : "rasultado"
+              } para:`
+            : "Não encontramos resultados para:"}
+        </span>
+        <h2>{title}</h2>
+      </TextResults>
+
+      <SearchNews>
+        {news.map((item) => (
+          <Card
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            banner={item.banner}
+            likes={item.likes}
+            comments={item.comments}
+          />
+        ))}
+      </SearchNews>
+    </ContainerResults>
+  );
 }
--- spa-breakignews/src/services/newsServices.js
+++ spa-breakignews/src/services/newsServices.js
@@ -11,3 +11,8 @@ export function getTopNews() {
     const response = axios.get(`${baseURL}/news/top`);
     return response;
 };
+
+export function searchNews(title){
+    const response = axios.get(`${baseURL}/news/search?title=${title}`);
+    return response;
+}
