87000c3a47cb0b17308b8846781ddfbc4b9e65d5
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-28 20:57:34 -0300

    feat: Update Input component to use react hook form and add zod resolver.

--- spa-breakignews/src/components/Input/Input.jsx
+++ spa-breakignews/src/components/Input/Input.jsx
@@ -1,8 +1,8 @@
 import { InputSpace } from "./InputStyled";
 
 
-export function Input({ type, placeholder, name}) {
+export function Input({ type, register, placeholder, name}) {
   return (
-    <InputSpace type={type} placeholder={placeholder}  name={name}/>
+    <InputSpace type={type} placeholder={placeholder}  {...register(name)}/>
   );
 }
