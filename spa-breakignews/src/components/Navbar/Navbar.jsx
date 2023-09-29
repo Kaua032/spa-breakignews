39ad849349ef9933af31a1e2b2cac3c768edc0b3
Kauã Xavier <kauaxavier032@gmail.com>
2023-09-28 22:54:22 -0300

    feat: Update Navbar component and styles, add Authentication page

--- spa-breakignews/src/components/Navbar/Navbar.jsx
+++ spa-breakignews/src/components/Navbar/Navbar.jsx
@@ -2,19 +2,9 @@ import { Link, Outlet, useNavigate } from "react-router-dom";
 import logo from "../../images/LogoBN.png";
 import { ImageLogo, InputSpace, Nav, ErrorSpan } from "./NavbarStyled";
 import { useForm } from "react-hook-form";
-import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "../Button/Button";
-// import './Navbar.css'
-
-const searchSchema = z.object({
-  title: z
-    .string()
-    .nonempty({ message: "A pesquisa não pode ser vazia." })
-    .refine((value) => !/^\s*$/.test(value), {
-      message: "A pesquisa não pode ter apenas espaços.",
-    }),
-});
+import { searchSchema } from "../../schemas/searchSchema";
 
 export function Navbar() {
   const {
--- spa-breakignews/src/pages/Authentication/Authentication.jsx
+++ spa-breakignews/src/pages/Authentication/Authentication.jsx
@@ -3,19 +3,26 @@ import { Button } from "../../components/Button/Button";
 import { Input } from "../../components/Input/Input";
 import { AuthContainer, Section } from "./AuthenticationStyled";
 import { useForm } from "react-hook-form";
+import { signinSchema } from "../../schemas/signinSchema";
+import { ErrorSpan } from "../../components/Navbar/NavbarStyled";
+import { signupSchema } from "../../schemas/signupSchema";
 
 export function Authentication() {
   const {
     register: registerSignup,
     handleSubmit: handleSubmitSignup,
     formState: { errors: errorsSignup },
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(signupSchema)
+  });
 
   const {
     register: registerSignin,
     handleSubmit: handleSubmitSignin,
     formState: { errors: errorsSignin },
-  } = useForm();
+  } = useForm({
+    resolver: zodResolver(signinSchema)
+  });
 
   function inHandleSubmit(data) {
     console.log(data);
@@ -36,12 +43,14 @@ export function Authentication() {
             name="email"
             register={registerSignin}
           />
+          {errorsSignin.email && <ErrorSpan>{errorsSignin.email.message}</ErrorSpan>}
           <Input
             type="password"
             placeholder="Senha"
             name="password"
             register={registerSignin}
           />
+          {errorsSignin.password && <ErrorSpan>{errorsSignin.password.message}</ErrorSpan>}
           <Button type="submit" text="Entrar"></Button>
         </form>
       </Section>
@@ -54,24 +63,29 @@ export function Authentication() {
             name="name"
             register={registerSignup}
           />
+          {errorsSignup.name && <ErrorSpan>{errorsSignup.name.message}</ErrorSpan>}
           <Input
             type="email"
             placeholder="E-mail"
             name="email"
             register={registerSignup}
           />
+          {errorsSignup.email && <ErrorSpan>{errorsSignup.email.message}</ErrorSpan>}
           <Input
             type="password"
             placeholder="Senha"
             name="password"
             register={registerSignup}
           />
+          {errorsSignup.password && <ErrorSpan>{errorsSignup.password.message}</ErrorSpan>}
           <Input
             type="password"
             placeholder="Confirmar senha"
-            name="password"
+            name="confirmPassword"
             register={registerSignup}
           />
+          {errorsSignup.confirmPassword && <ErrorSpan>{errorsSignup.confirmPassword.message}</ErrorSpan>}
+
           <Button type="submit" text="Cadastrar"></Button>
         </form>
       </Section>
